ccc745b915f91e476b48bc30dfdfc2f79f158381
kendpr <real.kendpr@gmail.com>
2024-01-02 22:16:43 +0800

    Made calculator class in js Modified the variable names Used the querySelectorAll instead of getElementsByClassName

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
+// calculator class functions
+
+class Calculator {
+  constructor(firstInputTextElement, secondInputTextElement) {
+    this.firstInputTextElement = firstInputTextElement;
+    this.secondInputTextElement = secondInputTextElement;
+    this.clearAll();
+  }
+
+  clearAll() {
+    this.firstInput = "";
+    this.secondInput = "";
+    this.operator = undefined;
+  }
+
+  appendNumber(number) {
+    this.firstInput = this.firstInput.toString() + number.toString();
+    // this.firstInput = number;
+  }
+
+  operation(operator) {}
+
+  evaluate() {}
+
+  updateInputs() {
+    this.firstInputTextElement.innerText = this.firstInput;
+  }
+}
+
 //Getting all needed elements from the html
 
-const numbers = document.getElementsByClassName("number");
+const numberBtns = document.querySelectorAll(".number");
 
-const operators = document.getElementsByClassName("operator");
+const operatorBtns = document.querySelectorAll(".operator");
 
 const negativeSwitcher = document.getElementById("negative-swithcher");
 
 const evalButton = document.getElementById("eval-btn");
 
-const firstInput = document.getElementById("first-input");
+const firstInputTextElement = document.getElementById("first-input");
 
-const secondInput = document.getElementById("second-input");
+const secondInputTextElement = document.getElementById("second-input");
 
 // console.log(numbers);
 
-//
+//Creating new calculator
+const calculator = new Calculator(
+  firstInputTextElement,
+  secondInputTextElement
+);
+
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateInputs();
+  });
+});
