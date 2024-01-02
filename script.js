d03f7967532106b52118dbc043758b18aaf360e9
kendpr <real.kendpr@gmail.com>
2024-01-02 19:23:03 +0800

    Added JS Targeted the following: - Number buttons - Operators (+,-,/,*) - Input Fields - Other buttons (Equal and clear all buttons)

---
+++ script.js
@@ -0,0 +1,17 @@
+//Getting all needed elements from the html
+
+const numbers = document.getElementsByClassName("number");
+
+const operators = document.getElementsByClassName("operator");
+
+const negativeSwitcher = document.getElementById("negative-swithcher");
+
+const evalButton = document.getElementById("eval-btn");
+
+const firstInput = document.getElementById("first-input");
+
+const secondInput = document.getElementById("second-input");
+
+// console.log(numbers);
+
+//
